c68385f0e03306a6847b286f49cb27774814fda3
Treepay-PC <treepay-pc@Treepay-PCs-MacBook-Air.local>
2017-10-11 15:16:50 +0700

    first hellow world

--- src/main/java/com/nattav/controllers/HomeController.java
+++ src/main/java/com/nattav/controllers/HomeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class HomeController {
 	
 	
-	@RequestMapping(value="/",method= RequestMethod.GET)
+	@RequestMapping(value="/")
 	public ModelAndView indexindex2() throws Exception{
 		
 		
